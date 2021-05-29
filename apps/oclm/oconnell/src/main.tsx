f0a7a6702a6849bc41d7ec8a2b20228b406fc34f
ramprits <ramprits-sdei@gmail.com>
2021-05-29 22:56:05 +0530

    Add AuthContext

--- apps/oclm/oconnell/src/main.tsx
+++ apps/oclm/oconnell/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
+
+import { AuthProvider } from './context/authContext';
 import App from './app/app';
 const theme = createMuiTheme();
 
@@ -12,7 +14,11 @@ ReactDOM.render(
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <HashRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </HashRouter>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
