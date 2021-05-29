04195971123190f95112e3381ace9faf694651db
ramprits <ramprits-sdei@gmail.com>
2021-05-29 23:05:07 +0530

    Add home design

--- apps/oclm/oconnell/src/app/home/home.tsx
+++ apps/oclm/oconnell/src/app/home/home.tsx
@@ -1,17 +1,15 @@
 import { Header } from '@my-org/shared-ui';
+import Home from '../../components/home/home';
 
 import './home.module.scss';
 
 /* eslint-disable-next-line */
 export interface HomeProps {}
 
-export function Home(props: HomeProps) {
+export function HomePage(props: HomeProps) {
   return (
-    <Header
-      bucketMain={[<h1>Welcome to home!</h1>]}
-      content={{ user: 'Ramprit sahani' }}
-    />
+    <Header bucketMain={[<Home />]} content={{ user: 'Ramprit sahani' }} />
   );
 }
 
-export default Home;
+export default HomePage;
